761e8089dc60318f419e7eadcaaea93060528019
Alexander Drach <alexander-drach@mail.ru>
2020-07-07 19:59:11 +0600

    ДЗ-6, добавление задач по кнопке

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const taskList = document.getElementById('tasks__list');
+const [btnAdd] = Array.from(document.getElementsByClassName('tasks__add'));
 
-const addTask = (e) => {
+const addTaskEnter = (e) => {
     const [form] = document.getElementsByClassName('tasks__control');
     form.addEventListener('submit', e => e.preventDefault());
     const input = document.getElementById('task__input');
@@ -31,7 +32,38 @@ const addTask = (e) => {
     }    
 }
 
+const addTaskBtn = (e) => {
+    const [form] = document.getElementsByClassName('tasks__control');
+    form.addEventListener('submit', e => e.preventDefault());
+    const input = document.getElementById('task__input');
+
+        if (input.value) {
+            taskList.insertAdjacentHTML('beforeEnd', 
+            `<div class="task">
+                <div class="task__title">
+                ${input.value}
+                </div>
+                <a href="#" class="task__remove">&times;</a>
+            </div>`            
+            );
+            input.value = '';
+
+            const closes = Array.from(document.getElementsByClassName('task__remove'));
+
+            closes.forEach( item => {
+                item.onclick = (e) => {
+                    e.preventDefault();
+                    item.closest('.task').remove();
+                }
+            })
+        }
+}
+
 document.onkeydown = (e) => {
-    addTask(e);
+    addTaskEnter(e);
+}
+
+btnAdd.onclick = (e) => {
+    addTaskBtn(e);
 }
 
